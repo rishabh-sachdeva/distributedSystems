6d7b4505e9b1d5b48ede5050a0476d1b86904812
rishabh-sachdeva <rishabh.sachdeva0808@gmail.com>
2019-02-28 04:04:49 -0500

    response channel fix

--- src/computeAvg/average.go
+++ src/computeAvg/average.go
@@ -1,50 +1,51 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
 	"sync"
-    "encoding/json"
-    "os"
-    "math"
-    "strings"
-    "strconv"
 )
-//var wg sync.WaitGroup 	
+
+//var wg sync.WaitGroup
 
 //JSON message struct
-type Message struct{
+type Message struct {
 	Start int
-	End int
+	End   int
 	Fname string
 }
 
-type Response struct{
-	Psum int
+type Response struct {
+	Psum   int
 	Pcount int
 	Prefix string
 	Suffix string
-	Start int
-	End int
+	Start  int
+	End    int
 }
 
 var wg sync.WaitGroup
 
-func coordinator(fileName string, M int){
+func coordinator(fileName string, M int) {
 
 	startPos := 0
 	//fragmentSize := 3
 	file, err := os.Open("F:/OperatingSystems/workspace/distributedSystems/src/computeAvg/data.txt")
 
-         if err != nil {
-                 fmt.Println(err)
-                 os.Exit(1)
-         }
-         defer file.Close()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer file.Close()
 
-    fileInfo, _ := file.Stat()
+	fileInfo, _ := file.Stat()
 
 	var file_size int64 = fileInfo.Size()
-	fmt.Println("file size",file_size)
+	fmt.Println("file size", file_size)
 
 	//dividing file into M chunks
 	fragment_size := math.Floor(float64(file_size-1) / float64(M))
@@ -53,137 +54,137 @@ func coordinator(fileName string, M int){
 	responseChannel := make(chan []byte)
 
 	//creating M workers
-	for i:=0; i< M; i++ {
+	for i := 0; i < M; i++ {
 		wg.Add(1)
-		msg := &Message{startPos,startPos+int(fragment_size),fileName}
+		msg := &Message{startPos, startPos + int(fragment_size), fileName}
 		marshalledMsg, _ := json.Marshal(msg) // message packing
-		channel := make(chan []byte,1)
-      // posting request message in channel
-     go func(){
-		channel <- marshalledMsg
-		close(channel)
-     }()
-     startPos = startPos + int(fragment_size)+1;
+		channel := make(chan []byte, 1)
+		// posting request message in channel
+		go func() {
+			channel <- marshalledMsg
+			close(channel)
+		}()
+		startPos = startPos + int(fragment_size) + 1
 
-	  //initiate job for a worker
-	  go worker(fileName, channel,responseChannel)
-	  
-	  marshalledRes := <- responseChannel
-	  unmarshalledRes := Response{}
-	  json.Unmarshal(marshalledRes, &unmarshalledRes)
-	  fmt.Println("resp ch", unmarshalledRes)
+		//initiate job for a worker
+		go worker(fileName, channel, responseChannel)
 	}
+	marshalledRes := <-responseChannel
+	unmarshalledRes := Response{}
+	json.Unmarshal(marshalledRes, &unmarshalledRes) // unpacking response
+	fmt.Println("resp ch", unmarshalledRes)
+
 }
 
-func worker(fileName string,channel chan []byte, responseChannel chan []byte){
+func worker(fileName string, channel chan []byte, responseChannel chan []byte) {
 	//create a place "msg" where the decoded data will be stored
 	defer wg.Done()
-	 marshalledMsg := <-channel
-	 unmarshalledMsg := Message{}
-	 json.Unmarshal(marshalledMsg, &unmarshalledMsg)// unpacking request message
-     response := calculateSum(unmarshalledMsg.Start,unmarshalledMsg.End,fileName)
-     marshalledResponse,_ := json.Marshal(response) // response packing
-     // sent res to coordinator via channel
-     //responseChannel := make(chan []byte,1)
-     go func(){
-     	//defer wg.Done()
-	    responseChannel <- marshalledResponse
-	    //close(responseChannel)
-     }()
+	marshalledMsg := <-channel
+	unmarshalledMsg := Message{}
+	json.Unmarshal(marshalledMsg, &unmarshalledMsg) // unpacking request message
+	response := calculateSum(unmarshalledMsg.Start, unmarshalledMsg.End, fileName)
+	marshalledResponse, _ := json.Marshal(response) // response packing
+	// sent res to coordinator via channel
+	//responseChannel := make(chan []byte,1)
+	go func() {
+		//defer wg.Done()
+		responseChannel <- marshalledResponse
+		//close(responseChannel)
+	}()
 }
 
-func calculateSum(start int, end int, fileName string) *Response{
-	sum:=0
+func calculateSum(start int, end int, fileName string) *Response {
+	sum := 0
 	file, err := os.Open(fileName)
 	checkIfAnyError(err)
-    defer file.Close()
-    _,e :=file.Seek(int64(start), 0) //check
+	defer file.Close()
+	_, e := file.Seek(int64(start), 0) //check
 	checkIfAnyError(e)
-	
+
 	fileContentForThisWorker := make([]byte, byte(end-start+1))
-	_,e1 := file.Read(fileContentForThisWorker)
-	
+	_, e1 := file.Read(fileContentForThisWorker)
+
 	checkIfAnyError(e1)
-	chunk:=string(fileContentForThisWorker)
-	
+	chunk := string(fileContentForThisWorker)
+
 	nos := strings.Fields(chunk)
-	fmt.Println(nos,len(nos))
+	fmt.Println(nos, len(nos))
 	nums := []int{}
-	
-	prefix,suffix:=processChunkString(chunk)
-	
+
+	prefix, suffix := processChunkString(chunk)
+
 	//initialize prefix, suffix and error variables
 	//pre :=0
 	//suf :=0
 	//var ep,es error
-	if len(prefix)>0{
+	if len(prefix) > 0 {
 		prefix = strings.TrimSpace(prefix)
 		//fmt.Println("inside len", prefix)
-		if len(prefix)<1{
-			prefix=" "
+		if len(prefix) < 1 {
+			prefix = " "
 		}
 	}
-	if(len(suffix)>0){
-		suffix=strings.TrimSpace(suffix)
+	if len(suffix) > 0 {
+		suffix = strings.TrimSpace(suffix)
 
-		if len(suffix)==0{
-			suffix=" "
+		if len(suffix) == 0 {
+			suffix = " "
 		}
 		//suf,es = strconv.Atoi(suffix)
 		//checkIfAnyError(es)
 	}
-	for _,i := range nos{
+	for _, i := range nos {
 		i = strings.TrimSpace(i)
-		j, err := strconv.Atoi(strings.Trim(i,"\x00")) // remove any null characters from string version of integer
+		j, err := strconv.Atoi(strings.Trim(i, "\x00")) // remove any null characters from string version of integer
 		checkIfAnyError(err)
-		nums = append(nums,j)
+		nums = append(nums, j)
 	}
-	st:=1
-	en:=len(nums)-1
-	if prefix == " "{
-		st =0
+	st := 1
+	en := len(nums) - 1
+	if prefix == " " {
+		st = 0
 	}
-	if suffix == " "{
-		en=len(nums)
+	if suffix == " " {
+		en = len(nums)
 	}
-	for i:=st;i<en;i++{
+	for i := st; i < en; i++ {
 		sum += nums[i]
 	}
-	res := &Response{Psum:sum, Pcount: len(nums)-2, Prefix:prefix , 
-		Suffix: suffix,Start: start, End: end}
+	res := &Response{Psum: sum, Pcount: len(nums) - 2, Prefix: prefix,
+		Suffix: suffix, Start: start, End: end}
 	return res
 }
 
-func processChunkString(chunk string) (string,string){
+func processChunkString(chunk string) (string, string) {
 	prefix := ""
 	suffix := ""
-	if strings.HasPrefix(chunk," "){
+	if strings.HasPrefix(chunk, " ") {
 		prefix = " "
 	}
-	if strings.HasSuffix(chunk, " "){
-		suffix =" "
+	if strings.HasSuffix(chunk, " ") {
+		suffix = " "
 	}
 	tokens := strings.Split(chunk, " ")
-	if prefix=="" {
+	if prefix == "" {
 		prefix = tokens[0]
 	}
-	if suffix=="" {
+	if suffix == "" {
 		suffix = tokens[len(tokens)-1]
 	}
 	//nums := []string{}
-	return prefix,suffix
+	return prefix, suffix
 }
 
-func checkIfAnyError(err error){
+func checkIfAnyError(err error) {
 	if err != nil {
-                 fmt.Println(err)
-                 os.Exit(1)
-         }
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
-func main(){
+func main() {
 	fileName := "F:/OperatingSystems/workspace/distributedSystems/src/computeAvg/data.txt"
 	//fileName := "data.txt"
 	//a := []int{7, 2, 8, -9, 4, 0}
-	coordinator(fileName,4)
-    wg.Wait()
+	coordinator(fileName, 4)
+	wg.Wait()
 }
