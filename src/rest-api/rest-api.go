60c64039d6d9043a7dd0c8c1aae92e4c93331bf6
Nipun <ramagiri.nipun@gmail.com>
2019-03-10 22:54:35 -0400

    Actual Updated Files

--- src/rest-api/rest-api.go
+++ src/rest-api/rest-api.go
@@ -11,12 +11,8 @@ import (
 	"github.com/gorilla/mux"
 )
 
-type Sites struct {
-	Sites []Site `json:"Sites"`
-}
-
 type Site struct {
-	Name         string        `json:"Name,omitempty"`github
+	Name         string        `json:"Name,omitempty"`
 	Role         string        `json:"Role,omitempty"`
 	URI          string        `json:"URI,omitempty"`
 	Accesspoints []AccessPoint `json:"Accesspoints,omitempty"`
@@ -29,47 +25,95 @@ type AccessPoint struct {
 
 var apts []AccessPoint
 var siteArr []Site
-var sites Sites
 
 func main() {
-	// apts = append(apts, AccessPoint{Label: "label-1", URL: "url-1"})
-	// apts = append(apts, AccessPoint{Label: "label-2", URL: "url-2"})
-
-	// siteArr = append(siteArr, Site{Name: "site-1", Role: "role-1", URI: "uri-1", Accesspoints: apts})
-	// siteArr = append(siteArr, Site{Name: "site-2", Role: "role-2", URI: "uri-2", Accesspoints: apts})
-
-	// Open our jsonFile
-	jsonFile, err := os.Open("Sites.json")
-	// if we os.Open returns an error then handle it
+	// Opening Persistent storage file
+	jsonFile, err := os.Open("sites.json")
 	if err != nil {
-		fmt.Println(err)
+		panic(err)
 	}
-
-	fmt.Println("Successfully Opened users.json")
-	// defer the closing of our jsonFile so that we can parse it later on
 	defer jsonFile.Close()
 
-	// read our opened xmlFile as a byte array.
-	byteValue, _ := ioutil.ReadAll(jsonFile)
-
-	// we unmarshal our byteArray which contains our
-	// jsonFile's content into 'users' which we defined above
-	json.Unmarshal(byteValue, &sites)
-	siteArr = sites.Sites[:]
+	b, _ := ioutil.ReadAll(jsonFile)
+	json.Unmarshal(b, &siteArr)
 
 	router := mux.NewRouter()
 	router.HandleFunc("/", projectIndex).Methods("GET")
+
+	// Sites APIs
+	// Create API
+	router.HandleFunc("/sites/{name}", CreateSite).Methods("PUT")
+	router.HandleFunc("/sites", CreateSite).Methods("POST")
+	// Read API
 	router.HandleFunc("/sites", GetSites).Methods("GET")
 	router.HandleFunc("/sites/{name}", GetSite).Methods("GET")
-	router.HandleFunc("/sites/{name}", CreateSite).Methods("POST")
+	// Update API
+	router.HandleFunc("/sites/{name}", UpdateSite).Methods("POST")
+	// Delete API
 	router.HandleFunc("/sites/{name}", DeleteSite).Methods("DELETE")
-	router.HandleFunc("/sites/{name}", UpdateSite).Methods("PUT")
+
+	// Access Points APIs
+	// Create API
+	router.HandleFunc("/sites/{name}/accesspoints/{label}", CreateAP).Methods("PUT")
+	router.HandleFunc("/sites/{name}/accesspoints", CreateAP).Methods("POST")
+	// Read API
+	router.HandleFunc("/sites/{name}/accesspoints", GetAPs).Methods("GET")
+	router.HandleFunc("/sites/{name}/accesspoints/{label}", GetAP).Methods("GET")
+	// Update API
+	router.HandleFunc("/sites/{name}/accesspoints/{label}", UpdateAP).Methods("POST")
+	// Delete API
+	router.HandleFunc("/sites/{name}/accesspoints/{label}", DeleteAP).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8001", router))
 }
 
 func projectIndex(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Project 1 \n Group Submission \n Group members: \n\tAniruddha\n\tArshitha\n\tNipun Ramagiri\n\tRishab")
+	fmt.Fprintf(w, `<h1>Project 1</h1> 
+					<h2>Group Submission</h2> <br>
+					Group members: <br>
+					<br>Aniruddha<br>Arshitha<br>Nipun Ramagiri<br>Rishab`)
+}
+
+// Site API Handlers
+// CreateSite - Creates a new Site
+func CreateSite(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	fmt.Println("POST: ", r.Body)
+	var site Site
+	_ = json.NewDecoder(r.Body).Decode(&site)
+
+	if r.Method == "PUT" {
+		var newSite Site
+		newSite = site
+		newSite.Name = params["name"]
+		fmt.Println(&newSite)
+		var isChanged bool
+		for idx, site := range siteArr {
+			if site.Name == params["name"] {
+				newSite.Name = params["name"]
+				newSite.Accesspoints = site.Accesspoints
+				siteArr[idx] = newSite
+				isChanged = true
+				break
+			}
+		}
+		if !isChanged {
+			siteArr = append(siteArr, newSite)
+		}
+		fileWriter, _ := os.Create("sites.json")
+		json.NewEncoder(fileWriter).Encode(siteArr)
+		defer fileWriter.Close()
+		json.NewEncoder(w).Encode(newSite)
+		return
+	}
+
+	site.Accesspoints = nil
+	siteArr = append(siteArr, site)
+	fileWriter, _ := os.Create("sites.json")
+	json.NewEncoder(fileWriter).Encode(siteArr)
+	json.NewEncoder(w).Encode(site)
+	defer fileWriter.Close()
+
 }
 
 // GetSites - Returns all the Sites
@@ -88,32 +132,7 @@ func GetSite(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// CreateSite - Creates a new Site [Original]
-func CreateSite(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	fmt.Println("POST: ", r.Body)
-
-	var site Site
-	_ = json.NewDecoder(r.Body).Decode(&site)
-
-	site.Name = params["name"]
-	siteArr = append(siteArr, site)
-	json.NewEncoder(w).Encode(site)
-}
-
-// DeleteSite -
-func DeleteSite(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	for idx, site := range siteArr {
-		if site.Name == params["name"] {
-			siteArr = append(siteArr[:idx], siteArr[idx+1:]...)
-			break
-		}
-		//json.NewEncoder(w).Encode(siteArr)
-	}
-}
-
-// UpdateSite -
+// UpdateSite - Updates a single Site of Name: {name}
 func UpdateSite(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	var updatedSite Site
@@ -122,9 +141,181 @@ func UpdateSite(w http.ResponseWriter, r *http.Request) {
 	for idx, site := range siteArr {
 		if site.Name == params["name"] {
 			updatedSite.Name = params["name"]
+			updatedSite.Accesspoints = site.Accesspoints
 			siteArr[idx] = updatedSite
 			break
 		}
 	}
+	fileWriter, _ := os.Create("sites.json")
+	json.NewEncoder(fileWriter).Encode(siteArr)
+	defer fileWriter.Close()
 	json.NewEncoder(w).Encode(updatedSite)
+}
+
+// DeleteSite - Deletes a single Site of Name: {name}
+func DeleteSite(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for idx, site := range siteArr {
+		if site.Name == params["name"] {
+			siteArr = append(siteArr[:idx], siteArr[idx+1:]...)
+			break
+		}
+	}
+	fileWriter, _ := os.Create("sites.json")
+	json.NewEncoder(fileWriter).Encode(siteArr)
+	defer fileWriter.Close()
+}
+
+// Access Point API Handlers
+// CreateAP - Creates a new AP
+func CreateAP(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	fmt.Println("POST: ", r.Body)
+	var AP AccessPoint
+	_ = json.NewDecoder(r.Body).Decode(&AP)
+
+	// Finding the corresponding site
+	var corrSite Site
+	var corrSiteidx int
+	for idx, site := range siteArr {
+		if site.Name == params["name"] {
+			corrSite = site
+			corrSiteidx = idx
+		}
+	}
+
+	// If Site absent, no insertion
+	if len(corrSite.Name) == 0 {
+		return
+	}
+
+	if r.Method == "PUT" {
+		var newAP AccessPoint
+		newAP = AP
+		newAP.Label = params["label"]
+		fmt.Println(&newAP)
+		var isChanged bool
+		for idx, ap := range corrSite.Accesspoints {
+			if ap.Label == params["label"] {
+				corrSite.Accesspoints[idx] = newAP
+				isChanged = true
+				break
+			}
+		}
+		if !isChanged {
+			corrSite.Accesspoints = append(corrSite.Accesspoints, newAP)
+		}
+
+		siteArr[corrSiteidx] = corrSite
+		fileWriter, _ := os.Create("sites.json")
+		json.NewEncoder(fileWriter).Encode(siteArr)
+		defer fileWriter.Close()
+		json.NewEncoder(w).Encode(newAP)
+		return
+	}
+
+	siteArr[corrSiteidx].Accesspoints = append(siteArr[corrSiteidx].Accesspoints, AP)
+	fileWriter, _ := os.Create("sites.json")
+	json.NewEncoder(fileWriter).Encode(siteArr)
+	json.NewEncoder(w).Encode(corrSite)
+	defer fileWriter.Close()
+
+}
+
+// GetAPs - Returns all the AP
+func GetAPs(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for _, site := range siteArr {
+		if site.Name == params["name"] {
+			json.NewEncoder(w).Encode(site.Accesspoints)
+			return
+		}
+	}
+}
+
+// GetAP - Returns single AP of Label: {label}
+func GetAP(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for _, site := range siteArr {
+		if site.Name == params["name"] {
+			for _, ap := range site.Accesspoints {
+				if ap.Label == params["label"] {
+					json.NewEncoder(w).Encode(ap)
+					return
+				}
+			}
+		}
+	}
+}
+
+// UpdateAP - Updates a single AP of Label: {label}
+func UpdateAP(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	fmt.Println("POST: ", r.Body)
+	var AP AccessPoint
+	_ = json.NewDecoder(r.Body).Decode(&AP)
+
+	// Finding the corresponding site
+	var corrSite Site
+	var corrSiteidx int
+	for idx, site := range siteArr {
+		if site.Name == params["name"] {
+			corrSite = site
+			corrSiteidx = idx
+		}
+	}
+
+	// If Site absent, no insertion
+	if len(corrSite.Name) == 0 {
+		return
+	}
+
+	for idx, ap := range corrSite.Accesspoints {
+		if ap.Label == params["label"] {
+			corrSite.Accesspoints[idx] = AP
+		}
+	}
+
+	siteArr[corrSiteidx] = corrSite
+	fileWriter, _ := os.Create("sites.json")
+	json.NewEncoder(fileWriter).Encode(siteArr)
+	json.NewEncoder(w).Encode(corrSite)
+	defer fileWriter.Close()
+}
+
+// DeleteAP - Deletes a single AP of Label: {label}
+func DeleteAP(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	fmt.Println("POST: ", r.Body)
+	var AP AccessPoint
+	_ = json.NewDecoder(r.Body).Decode(&AP)
+
+	// Finding the corresponding site
+	var corrSite Site
+	var corrSiteidx int
+	for idx, site := range siteArr {
+		if site.Name == params["name"] {
+			corrSite = site
+			corrSiteidx = idx
+			break
+		}
+	}
+
+	// If Site absent, no insertion
+	if len(corrSite.Name) == 0 {
+		return
+	}
+
+	for idx, ap := range corrSite.Accesspoints {
+		if ap.Label == params["label"] {
+			corrSite.Accesspoints = append(corrSite.Accesspoints[:idx], corrSite.Accesspoints[idx+1:]...)
+			break
+		}
+	}
+
+	siteArr[corrSiteidx] = corrSite
+	fileWriter, _ := os.Create("sites.json")
+	json.NewEncoder(fileWriter).Encode(siteArr)
+	json.NewEncoder(w).Encode(corrSite)
+	defer fileWriter.Close()
 }
