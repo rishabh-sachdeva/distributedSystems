d265c1ca62ade71146b7ec24875c1051a3dfa766
rishabh-sachdeva <rishabh.sachdeva0808@gmail.com>
2019-05-11 05:51:04 +0530

    some fixes

--- src/gossip/ComputeAvgGossip.go
+++ src/gossip/ComputeAvgGossip.go
@@ -1,59 +1,93 @@
 package main
 
 import (
-	"fmt"
-     zmq "github.com/zeromq/goczmq"
+ 	"fmt"
+     zmq "github.com/pebbe/zmq4"
     "time"
+  	"strconv"
+
 )
-type Message_worker{
+
+type Message_worker struct{
 	SocketAddress string
 	Value int
 	Neighbour1Socket string
-	neighbour2Socket string 
+	Neighbour2Socket string 
 	
 }
 
-func worker(Message_worker message){
-	value := message.Value
+func worker(message *Message_worker){
+	//value := message.Value
+	//fmt.Println("Connecting")
+
 	my_address := message.SocketAddress
 	neighbour1 := message.Neighbour1Socket
 	neighbour2 := message.Neighbour2Socket
 	context,_ := zmq.NewContext()
-	zmq_socket = context.NewSocket(zmq.REQ)
-	zmq_socket.bind(my_address)
+	sub,_ := context.NewSocket(zmq.SUB)
+
+	sub.SetLinger(0)
+	defer sub.Close()
 	
-	if neighbour1 != nil{
-		
-	}
-	
-	
+	sub.Connect(neighbour1)
+	sub.Connect(neighbour2)
+	sub.SetSubscribe("")
+
+
+	go Listen(sub)
+
+	pub,_ := context.NewSocket(zmq.PUB)
+	pub.Bind(my_address)
+
+	for _ = range time.Tick(time.Second) {
+        pub.Send("test", 0)
+        fmt.Println("send", "test")
+    }
+	defer pub.Close()
+}
+
+func Listen(subscriber *zmq.Socket) {
+    for {
+        s, err := subscriber.Recv(0)
+        if err != nil {
+            fmt.Println(err)
+            continue
+        }
+        fmt.Println("receiving", s)
+    }
 }
 
 func main(){
 	num_nodes := 4
-	value_map = make(map[int]int)
+	value_map := make(map[int]int)
 	value_map[1]=10
 	value_map[2]=20
 	value_map[3]=30
 	value_map[4]=40
 	
-	var arr_nodes [num_nodes]int
+	var arr_nodes [4]int
 	arr_nodes[0] = 1
 	arr_nodes[1] = 2
 	arr_nodes[2] = 3
 	arr_nodes[3] = 4
 	
-	socket_address_map = make(map[int]string)
+	socket_address_map := make(map[int]string)
 	for i:=1; i<=num_nodes;i++{		
 		//assign addresses to node workers.
-		socket_address_map[i] = "tcp://127.0.0.1:555"+i
-		
+		port := 5550 + i
+		socket_address_map[i] = "tcp://127.0.0.1:"+strconv.Itoa(port)
+		fmt.Println(socket_address_map[i])
 					
 	}
-	for i:=1; i<=num_nodes;i++{
-		neigh1, ok := socket_address_map[i-1]
-		neigh2, ok := socket_address_map[i+1]
-		msg_worker := &Message_worker{socket_address_map[i],value_map[i],neigh1, neigh2}
+	for j:=1; j<=num_nodes;j++{
+		neigh1,_ := socket_address_map[j-1]
+		neigh2,_ := socket_address_map[j+1]
+		fmt.Println("calling worker")
+		msg_worker := &Message_worker{socket_address_map[j],value_map[j],neigh1, neigh2}
+		fmt.Println(*msg_worker)
 		go worker(msg_worker)
+		
 	}
+	time.Sleep(7 * time.Second)
+
 }
